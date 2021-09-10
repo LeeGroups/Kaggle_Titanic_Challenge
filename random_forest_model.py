5c80335e79f405072852459e325196f0160d6f7e
LeeGroups <haobillylee@gmail.com>
2021-09-10 12:51:01 -0500

    cleaned up the code of random forest

--- random_forest_model.py
+++ random_forest_model.py
@@ -6,34 +6,60 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import balanced_accuracy_score
 import matplotlib.pyplot as plt
 
-train_data = pd.read_csv("/Data/train.csv")
-test_data = pd.read_csv("/Data/test.csv")
- 
+# Import the training data
+train_data = pd.read_csv("./Data/train.csv")
+test_data = pd.read_csv("./Data/test.csv")
 
-X_train, X_test, y_train, y_test = train_test_split(train_data.drop(["Survived"], axis=1), train_data["Survived"], test_size=0.33, random_state=42)
+# Features we care about
+features = ["Pclass", "Sex", "SibSp", "Parch","Age"]
 
-features = ["Pclass", "Sex", "SibSp", "Parch"]
+# Limit the training to the only parameters we care about
+train_data = train_data[features + ["Survived"]]
+
+# delete the rows of corresponding to passengers that do not have an Age entry
+train_data.dropna(inplace=True)
+
+""" X_train, X_test, y_train, y_test = train_test_split(train_data.drop(["Survived"], axis=1), train_data["Survived"], test_size=0.33, random_state=42)
+
 X = pd.get_dummies(X_train[features])
-X_test_dummy = pd.get_dummies(X_test[features])
+X_test_dummy = pd.get_dummies(X_test[features]) """
 
-#model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-#model.fit(X, y_train)
-#predictions = model.predict(X_test_dummy)
+# Split the training data set, so that 1/4 of them are left as validation
+x_train, x_test, y_train, y_test = train_test_split(train_data.drop(["Survived"], axis=1), train_data["Survived"], test_size=0.25, random_state=42)
 
+X_train_dummy = pd.get_dummies(x_train[features])
+X_test_dummy = pd.get_dummies(x_test[features])
+
+
+# Defines a function that trains a RandomForestClassifier model using parameters depth and n_estimator, and outputs the accuracy of the model at predicting x against y
 def parametric_model(depth, n_estimators, x = X_test_dummy, y = y_test):
     model = RandomForestClassifier(n_estimators = n_estimators, max_depth=depth, random_state=1)
-    model.fit(X, y_train)
+    model.fit(X_train_dummy, y_train)
     predictions = model.predict(x)
     return accuracy_score(predictions,y)
 
+
+# Initialize the depths we want to test
 depths = range(1,10)
+
+accuracy_depth_test = [parametric_model(depth,100) for depth in depths]
+accuracy_depth_train = [parametric_model(depth,100, X_train_dummy, y_train) for depth in depths]
+
+# Plot the accuracy of the prediction of the validation (in blue) and training set (in green) with varying depths 
+plt.plot(depths, accuracy_depth_test,color='blue')
+plt.plot(depths, accuracy_depth_train,color='green')
+plt.show()
+
+
+# Initialize the n-estimates we want to test
 n_estimate = range(70,200,10)
 
-accuracy_test = [parametric_model(6,estimate) for estimate in n_estimate]
-accuracy_train = [parametric_model(6,estimate, X, y_train) for estimate in n_estimate]
+accuracy_est_test = [parametric_model(6,estimate) for estimate in n_estimate]
+accuracy_est_train = [parametric_model(6,estimate, X_train_dummy, y_train) for estimate in n_estimate]
 
-plt.plot(n_estimate, accuracy_test,color='blue')
-plt.plot(n_estimate,accuracy_train,color='green')
+# Plot the accuracy of the prediction of the validation (in blue) and training set (in green) with varying n_estimates
+plt.plot(n_estimate, accuracy_est_test,color='blue')
+plt.plot(n_estimate,accuracy_est_train,color='green')
 plt.show()
 
 #print(accuracy_score(predictions,y_test))
